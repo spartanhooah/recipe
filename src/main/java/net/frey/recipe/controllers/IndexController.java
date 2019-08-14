88aa1474ca56306650abaf04ca00b850e3728897
Ben Frey <spartanhooah@gmail.com>
2019-08-13 20:43:27 -0500

    add index controller and template

--- src/main/java/net/frey/recipe/controllers/IndexController.java
+++ src/main/java/net/frey/recipe/controllers/IndexController.java
@@ -1,4 +1,13 @@
 package net.frey.recipe.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        System.out.println("Something else 55555 to say");
+        return "index";
+    }
 }
