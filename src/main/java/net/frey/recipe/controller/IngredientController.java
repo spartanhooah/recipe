0a08c917458772ac93eced9a8afdfb28007bacbc
spartanhooah <spartanhooah@gmail.com>
2019-11-01 15:17:00 -0500

    can view ingredient list separately

---
+++ src/main/java/net/frey/recipe/controller/IngredientController.java
@@ -0,0 +1,23 @@
+package net.frey.recipe.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.frey.recipe.service.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+@AllArgsConstructor
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    @GetMapping(path = "/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable Long recipeId, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
+
+        return "recipe/ingredient/list";
+    }
+}
---
+++ src/test/java/net/frey/recipe/controller/IngredientControllerTest.java
@@ -0,0 +1,48 @@
+package net.frey.recipe.controller;
+
+import net.frey.recipe.command.RecipeCommand;
+import net.frey.recipe.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IngredientControllerTest {
+    private IngredientController ingredientController;
+    private MockMvc mockMvc;
+
+    @Mock
+    private RecipeService recipeService;
+
+    @Before
+    public void setUp() {
+        ingredientController = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(ingredientController).build();
+    }
+
+    @Test
+    public void listIngredients() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
